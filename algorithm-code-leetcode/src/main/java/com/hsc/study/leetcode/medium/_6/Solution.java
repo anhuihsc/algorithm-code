631befd564900709dd976ddd5e21f8fb2a4f20d1
wansong <shanchun.hsc@alibaba-inc.com>
2019-12-22 16:53:01 +0800

    fiux

---
+++ algorithm-code-leetcode/src/main/java/com/hsc/study/leetcode/medium/_6/Solution.java
@@ -0,0 +1,34 @@
+package com.hsc.study.leetcode.medium._6;
+
+import java.util.*;
+
+/**
+ * @author wansong
+ */
+public class Solution {
+    Map<String, String> phone = new HashMap<String, String>() {{
+        put("2", "abc");
+        put("3", "def");
+        put("4", "ghi");
+        put("5", "jkl");
+        put("6", "mno");
+        put("7", "pqrs");
+        put("8", "tuv");
+        put("9", "wxyz");
+    }};
+
+    public List<String> letterCombinations(String digits) {
+        if (digits == null) {
+            return Collections.emptyList();
+        }
+        int len=Double.valueOf(Math.pow(3.0,digits.length())).intValue();
+        List<String> ret = new ArrayList<String>(len);
+        for (int i = 0; i < digits.length(); i++) {
+            String value = phone.get(digits.charAt(i) + "");
+            if (value == null) {
+                continue;
+            }
+            for(int j=0;j<d      )
+        }
+    }
+}
--- algorithm-code-leetcode/src/main/java/com/hsc/study/leetcode/simple/_6/Solution.java
+++ algorithm-code-leetcode/src/main/java/com/hsc/study/leetcode/simple/_6/Solution.java
@@ -1,7 +1,9 @@
 package com.hsc.study.leetcode.simple._6;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
@@ -15,10 +17,21 @@ import java.util.List;
  * 因为 nums[0] + nums[1] = 2 + 7 = 9 所以返回 [0, 1]
  * <p>
  * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/two-sum 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * <p>
+ * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ * <p>
+ * 满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
+ * <p>
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/3sum 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        return violence(nums,0);
+        return violence(nums, 0);
     }
 
     /**
@@ -27,19 +40,28 @@ public class Solution {
 
     private List<List<Integer>> violence(int[] nums, int target) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Set<List<Integer>> set = new HashSet<List<Integer>>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i  +1; j < nums.length; j++) {
-                for (int k = i+1; k < nums.length; k++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
                     if (target == nums[i] + nums[j] + nums[k]) {
                         List<Integer> list = new ArrayList<Integer>(3);
-                        list.add(i);
-                        list.add(j);
-                        list.add(k);
-                        res.add(list);
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        if (!set.contains(list)) {
+                            set.add(list);
+                            res.add(list);
+                        }
                     }
                 }
             }
         }
         return res;
+    }
+
+    private void checkDup(int i, int j, int k) {
+        int tmp;
+
     }
 }
