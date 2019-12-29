b3def57adfaf3e3f8b0d4cc6394750f7d47d4756
wansong <shanchun.hsc@alibaba-inc.com>
2019-12-29 16:54:50 +0800

    fix

--- algorithm-code-leetcode/src/main/java/com/hsc/study/leetcode/medium/_8/Solution.java
+++ algorithm-code-leetcode/src/main/java/com/hsc/study/leetcode/medium/_8/Solution.java
@@ -18,6 +18,9 @@ import java.util.*;
 public class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return Collections.emptyList();
+        }
         return violence(nums, 0);
     }
 
@@ -33,6 +36,9 @@ public class Solution {
             numMap.put(nums[i], i);
         }
         for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > target) {
+                return res;
+            }
             for (int j = i + 1; j < nums.length - 1; j++) {
                 int val = target - nums[i] - nums[j];
                 if (numMap.containsKey(val) && numMap.get(val) > j) {
@@ -63,6 +69,35 @@ public class Solution {
         }
         return false;
     }
+
+
+
+    public static List<List<Integer>> threeSum1(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        int len = nums.length;
+        if(nums == null || len < 3) return ans;
+        Arrays.sort(nums); // 排序
+        for (int i = 0; i < len ; i++) {
+            if(nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+            if(i > 0 && nums[i] == nums[i-1]) continue; // 去重
+            int L = i+1;
+            int R = len-1;
+            while(L < R){
+                int sum = nums[i] + nums[L] + nums[R];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while (L<R && nums[L] == nums[L+1]) L++; // 去重
+                    while (L<R && nums[R] == nums[R-1]) R--; // 去重
+                    L++;
+                    R--;
+                }
+                else if (sum < 0) L++;
+                else if (sum > 0) R--;
+            }
+        }
+        return ans;
+    }
+
 
     public static void main(String[] args) {
         long time0=System.currentTimeMillis();
