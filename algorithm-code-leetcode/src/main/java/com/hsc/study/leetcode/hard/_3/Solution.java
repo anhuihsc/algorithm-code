6eaeca55cd3895859afa015d5999d3fdb9b15673
wansong <shanchun.hsc@alibaba-inc.com>
2020-01-18 17:18:02 +0800

    add

---
+++ algorithm-code-leetcode/src/main/java/com/hsc/study/leetcode/hard/_3/Solution.java
@@ -0,0 +1,23 @@
+package com.hsc.study.leetcode.hard._3;
+
+import com.hsc.study.leetcode.ListNode;
+
+/**
+ * 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [   1->4->5,   1->3->4,   2->6 ] 输出: 1->1->2->3->4->4->5->
+ * <p>
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/merge-k-sorted-lists 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        return null;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
