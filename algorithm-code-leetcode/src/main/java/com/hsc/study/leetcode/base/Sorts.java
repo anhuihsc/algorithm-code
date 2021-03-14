cbc6b60aa99edcf21827fa75075f6f3b94e5b8b6
皖松 <shanchun.hsc@alibaba-inc.com>
2021-03-14 20:11:57 +0800

    add

--- algorithm-code-leetcode/src/main/java/com/hsc/study/leetcode/base/Sorts.java
+++ algorithm-code-leetcode/src/main/java/com/hsc/study/leetcode/base/Sorts.java
@@ -7,14 +7,16 @@ package com.hsc.study.leetcode.base;
 public class Sorts {
 
     /**
-     * 一次冒泡会让至少一个元素移动到它应该在的位置
-     * 从小到大
+     * 一次冒泡会让至少一个元素移动到它应该在的位置，所以最坏情况需要扫描n次
+     * 只会操作相邻的两个数据，内循环是比较相邻的两个元素
      */
     public static void bubbleSort(int arr[]) {
         if (arr == null || arr.length <= 1) {
             return;
         }
         int len = arr.length;
+        //是否存在交换的标志位
+        boolean existSwap = false;
         //需要n次冒泡
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < len - i - 1; j++) {
@@ -23,8 +25,66 @@ public class Sorts {
                     int tmp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = tmp;
+
+                    existSwap = true;
                 }
             }
+            // 没有数据交换，提前退出
+            if (!existSwap) {
+                break;
+            }
+
+        }
+    }
+
+    /**
+     * 插入排序
+     *
+     */
+    public static void insertSort(int arr[]) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        int len = arr.length;
+        //
+        for (int i = 1; i < len; i++) {
+            int value = arr[i];
+            int j = i - 1;
+            // 和前面进行比较，数据后移动
+            for (; j >= 0; j--) {
+                //数据移动
+                if (arr[j] > value) {
+                    arr[j + 1] = arr[j];
+                } else {
+                    break;
+                }
+            }
+            //插入合适的数据
+            arr[j + 1] = value;
+        }
+    }
+
+    /**
+     * 选择排序
+     *
+     */
+    public static void selectSort(int arr[]) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        int len = arr.length;
+        // 每次选择一个元素
+        for (int i = 0; i < len; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < len; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            //交换选择出来的最小值,放到相应的位置
+            int tmp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = tmp;
         }
     }
 
@@ -35,8 +95,8 @@ public class Sorts {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 2, 4, 3, 7, 6, 8, 9, 12};
-        bubbleSort(arr);
+        int arr[] = {3, 2, 3, 4, 5, 6, 8, 9, 12};
+        selectSort(arr);
         print(arr);
     }
 
