11781aad3b25797f45abf3e44d4841e9fe56b995
皖松 <shanchun.hsc@alibaba-inc.com>
2021-03-20 16:45:50 +0800

    sort

--- algorithm-code-leetcode/src/main/java/com/hsc/study/leetcode/base/Sorts.java
+++ algorithm-code-leetcode/src/main/java/com/hsc/study/leetcode/base/Sorts.java
@@ -39,7 +39,6 @@ public class Sorts {
 
     /**
      * 插入排序
-     *
      */
     public static void insertSort(int arr[]) {
         if (arr == null || arr.length <= 1) {
@@ -66,7 +65,6 @@ public class Sorts {
 
     /**
      * 选择排序
-     *
      */
     public static void selectSort(int arr[]) {
         if (arr == null || arr.length <= 1) {
@@ -88,15 +86,100 @@ public class Sorts {
         }
     }
 
+    public static void mergeSort(int arr[]) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        mergeSortUseRecursion(arr, 0, arr.length - 1);
+    }
+
+    private static void mergeSortUseRecursion(int arr[], int p, int r) {
+        //递归结束条件
+        if (p >= r) {
+            return;
+        }
+        //取中间位置
+        int q = p + (r - p) / 2;
+        mergeSortUseRecursion(arr, p, q);
+        mergeSortUseRecursion(arr, q + 1, r);
+        //数据做merge(p,r)
+        int[] left = new int[q - p + 2];
+        int[] right = new int[r - q + 1];
+
+        for (int i = 0; i <= q - p; i++) {
+            left[i] = arr[p + i];
+        }
+        for (int i = 0; i < r - q; i++) {
+            right[i] = arr[q + 1 + i];
+        }
+        // 第一个数组添加哨兵（最大值）
+        left[q - p + 1] = Integer.MAX_VALUE;
+        // 第二个数组添加哨兵（最大值）
+        right[r - q] = Integer.MAX_VALUE;
+        int i = 0;
+        int j = 0;
+        int k = p;
+        while (k <= r) {
+            // 当左边数组到达哨兵值时，i不再增加，直到右边数组读取完剩余值，同理右边数组也一样
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+
+    }
+
     private static void print(int arr[]) {
         for (int a : arr) {
             System.out.println(a);
         }
     }
 
+    public static void quickSort(int arr[]) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        quickSortUseRecursion(arr, 0, arr.length-1);
+    }
+
+    private static void quickSortUseRecursion(int arr[], int p, int r) {
+        if (p >= r) {
+            return;
+        }
+        int q = partition(arr, p, r);
+        quickSortUseRecursion(arr, p, q);
+        quickSortUseRecursion(arr, q + 1, r);
+
+    }
+
+    private static int partition(int arr[], int p, int r) {
+        int pivot = arr[p];
+
+        int leftIndex = p;
+        for (int index = p + 1; index <= r; index++) {
+            //比哨兵大不用处理
+            if (arr[index] < pivot) {
+                leftIndex++;
+                //如果不是一个位置，需要把小的放到左边大的放到右边
+                if (leftIndex != index) {
+                    int tmp = arr[leftIndex];
+                    arr[leftIndex] = arr[index];
+                    arr[index] = tmp;
+                }
+            }
+        }
+        int tmp = arr[leftIndex];
+        arr[leftIndex] = arr[p];
+        arr[p] = tmp;
+        return leftIndex;
+    }
+
+
+
     public static void main(String[] args) {
-        int arr[] = {3, 2, 3, 4, 5, 6, 8, 9, 12};
-        selectSort(arr);
+        int arr[] = {1,3, 2, 13, 4, 5, 6, 8, 9, 12};
+        quickSort(arr);
         print(arr);
     }
 
